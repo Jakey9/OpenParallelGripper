e8e987e79160b30bff09955b995c2c7a13e3bc47
Jake <jake.tan@neura-robotics.com>
2026-07-14 16:14:26 +0800

    fix: updated sample python code

--- SCS3045M_version/software/examples/contorl_with_modbus_rtu.py
+++ SCS3045M_version/software/examples/contorl_with_modbus_rtu.py
@@ -16,30 +16,39 @@ class ParalleGripperAtom:
         self._arm.set_state(0)
         code = self._arm.set_tgpio_modbus_baudrate(115200)
         print('set_tgpio_modbus_baudrate, code={}'.format(code))
-        self.close_in_res = 200
-        self.open_in_res = 2200
+        # self.close_in_res = 200
+        # self.open_in_res = 2200
+        self.close_in_res = 1918
+        self.open_in_res = 839
+
+    @property
+    def pos_min(self):
+        return min(self.close_in_res, self.open_in_res)
+
+    @property
+    def pos_max(self):
+        return max(self.close_in_res, self.open_in_res)
 
     def open(self):
-        data = [0x01, 0x06, 0x00, 0x80, 0x08, 0x98]  # 2200
-        res = self._arm.getset_tgpio_modbus_data(data)
+        data = [0x01, 0x06, 0x00, 0x80] + int_to_2bytes_list(self.open_in_res)
+        self._arm.getset_tgpio_modbus_data(data)
         time.sleep(1)
 
     def close(self):
-        data = [0x01, 0x06, 0x00, 0x80, 0x00, 0xc8]  # 200
-        res = self._arm.getset_tgpio_modbus_data(data)
+        data = [0x01, 0x06, 0x00, 0x80] + int_to_2bytes_list(self.close_in_res)
+        self._arm.getset_tgpio_modbus_data(data)
         time.sleep(1)
 
     def move(self, pos):
-        assert pos >= self.close_in_res
-        assert pos <= self.open_in_res
+        assert self.pos_min <= pos <= self.pos_max
 
         data = [0x01, 0x06, 0x00, 0x80] + int_to_2bytes_list(pos)
-        res = self._arm.getset_tgpio_modbus_data(data)
+        self._arm.getset_tgpio_modbus_data(data)
         time.sleep(1)
 
     def move_out_of_10(self, ratio):
-        assert ratio <= 9
-        pos = (self.open_in_res - self.close_in_res) * ratio / 10 + self.close_in_res
+        assert 0 <= ratio <= 9
+        pos = self.open_in_res + (self.close_in_res - self.open_in_res) * ratio / 9
         self.move(int(pos))
 
     def get_pos(self):
